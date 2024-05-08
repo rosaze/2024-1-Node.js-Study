42a06d9b4880571b13d66f2c2ba2195088ba33d9
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2024-05-08 19:29:07 +0900

    Feat: 4주차 필기

--- 정민주/code/func.mjs
+++ 정민주/code/func.mjs
@@ -1,4 +1,5 @@
 import {odd, even} from './var.mjs'
+console.log('This is func.mjs');
 
 function checkEvenOrOdd(num){
     if (num%2){
---
+++ 정민주/code/timer.js
@@ -0,0 +1,17 @@
+function sayHello(){
+    console.log("Hi Everyone")
+}
+function sayBye(){
+    console.log("Goodbye Everyone")
+}
+
+const bye = setTimeout(sayBye, 2000)
+setTimeout(()=>{clearTimeout(bye)}, 100)
+//clearTimeout(bye);
+
+const hello = setInterval(sayHello, 5000)
+setTimeout(()=>{clearInterval(hello)}, 15000)
+
+const immediateBye = setImmediate(sayBye)
+clearImmediate(immediateBye)
+
