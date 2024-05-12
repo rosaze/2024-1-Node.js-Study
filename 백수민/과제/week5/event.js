b9767dc9595c4ce37bfc8c1f315d4989fd5550fc
suminb99 <suminbaek99@gmail.com>
2024-05-12 22:53:34 +0900

    Feat: 5주차 과제

--- 백수민/과제/week5/event.js
+++ 백수민/과제/week5/event.js
@@ -1,1 +1,38 @@
 // 커스텀 이벤트 만들어보기
+
+const EventEmitter = require('events');
+
+const myEvent = new EventEmitter();
+
+myEvent.addListener('greet', () => {
+    console.log('Hello!');
+});
+
+myEvent.on('greet_back', () => {
+    console.log('Hi');
+});
+
+myEvent.on('greet_back', () => {
+    console.log('Nice to to meet you!');
+});
+
+myEvent.on('say_goodbye', () => {
+    console.log('Me too');
+});
+
+myEvent.on('say_goodbye', () => {
+    console.log('It was nice talking to you, bye');
+});
+
+myEvent.on('say_bye_back', () => {
+    console.log('Bye');
+});
+
+myEvent.on('say_bye_back', () => {
+    console.log('see you later');
+});
+
+myEvent.emit('greet');
+myEvent.emit('greet_back');
+myEvent.emit('say_goodbye');
+myEvent.emit('say_bye_back');
