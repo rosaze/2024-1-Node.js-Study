77f68b6e2a7f87fa7c0640235ae182a4602b6633
seobin0224 <163140905+seobin0224@users.noreply.github.com>
2024-05-14 17:01:15 +0900

    Delete 정서연 directory

--- 정서연/assignment/week4/index.js
+++
@@ -1,9 +0,0 @@
-const calculator = require("./calculator");
-
-const a = 10;
-const b = 5;
-
-console.log(`Addition: ${calculator.add(a, b)}`);
-console.log(`Subtraction: ${calculator.subtract(a, b)}`);
-console.log(`Multiplication: ${calculator.multiply(a, b)}`);
-console.log(`Division: ${calculator.divide(a, b)}`);
