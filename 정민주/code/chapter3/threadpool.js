f59fbb22593f408d75aecc699f3aeade5b458091
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2024-05-15 18:33:52 +0900

    Feat: 5주차 필기와 과제
    명함 추가하는 서버와 프런트 만듦

---
+++ 정민주/code/chapter3/threadpool.js
@@ -0,0 +1,43 @@
+const crypto = require('crypto')
+process.env.UV_THREADPOOL_SIZE = 3;
+
+const pass = 'pass'
+const salt = 'salt'
+const start = Date.now()
+
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('1', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('2', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('3', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('4', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('5', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('6', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('7', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('8', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('9', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('10', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('11', Date.now() - start)
+})
+crypto.pbkdf2(pass,salt,1_000_000, 128, 'sha512', ()=>{
+    console.log('12', Date.now() - start)
+})
---
+++ 정민주/note/week5.md
@@ -0,0 +1,2 @@
+https://mnevermore1122.tistory.com/181
+비밀번호: 1122
